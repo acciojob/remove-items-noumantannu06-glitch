969928b3611fa2f916ed5a73f0382e7857677876
Md Nouman <noumantannu06@gmail.com>
2026-04-10 20:28:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const selectElement = document.getElementById('your-dropdown-id');
-const selectedOptions = Array.from(selectElement.selectedOptions);
-
-selectedOptions.forEach(option => {
-  selectElement.removeChild(option);
-});
+ function removecolor() {
+            var x = document.getElementById("colorSelect");
+            if (x && x.selectedIndex >= 0) {
+                x.removeChild(x.options[x.selectedIndex]);
+            }
+        }
